7debb8277a934d93e8535ac8f94fa538be01fe34
Hardik <hardik.dholakiya@arsenaltech.com>
2017-05-16 15:49:12 +0530

    Update

--- resources/views/error/404.blade.php
+++ resources/views/error/404.blade.php
@@ -4,15 +4,7 @@
 @endsection
 @section('content')
     <div align="center" style="background-color: #FFFFFF">
-        {{--@if($error_message)--}}
-            <h2> Exception
-            </h2>
-            <h4>
-                {{--{{$error_message}}--}}
-            </h4>
-        {{--@else--}}
             <img src="{{asset('image/404 page.jpg')}}"><br>
             <h3>Error file not found.!!!</h3>
-        {{--@endif--}}
     </div>
 @endsection
