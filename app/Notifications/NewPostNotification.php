843def3695a0c5423132214c40055ad16b5cf198
Hardik <hardik.dholakiya@arsenaltech.com>
2017-06-21 13:15:11 +0530

    when post  create that time send notification  to user

---
+++ app/Notifications/NewPostNotification.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\SlackMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class NewPostNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    protected $post;
+    public function __construct($post)
+    {
+        $this->post=$post;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('You Create now post.')
+                    ->line('Post Title is "'.$this->post->title.'"')
+                    ->action('Click For view post', url('/').'/showPost/'.$this->post->post_id)
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+    public function toSlack($notifiable)
+    {
+        return (new SlackMessage)
+            ->content('One of your invoices has been paid!');
+    }
+}
